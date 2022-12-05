fe26fcc2e2375b300c1dd5b07485ff533b556e1e
Mike <mike@mike-francis.org>
2022-12-05 09:01:57 +0000

    template commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      Yarn React ReactRouter Tailwind Typescript template
+    </div>
+  )
+}
+
+export default App
