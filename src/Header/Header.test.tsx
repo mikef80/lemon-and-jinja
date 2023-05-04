d9d4895d2e07478666adc7a94938262ae9b7553f
Mike <mike@mike-francis.org>
2023-05-04 22:35:35 +0100

    working on extra tests

--- src/Header/Header.test.tsx
+++ src/Header/Header.test.tsx
@@ -8,7 +8,9 @@ test("should load header", async () => {
 
   // ACT
   await screen.findByRole("heading");
+  await screen.findByRole("heading2");
 
   // ASSERT
   expect(screen.getByRole("heading")).toHaveTextContent("Lemon + Jinja");
+  expect(screen.getByRole("heading2")).toHaveTextContent("heading 2");
 });
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div role='heading' className='font-league_spartan text-4xl uppercase font-bold text-ljblue'>Lemon + Jinja</div>
-  )
-}
+    <>
+      <div
+        role="heading"
+        className="font-league_spartan text-4xl uppercase font-bold text-ljblue"
+      >
+        Lemon + Jinja
+      </div>
+      <div
+        role="heading2"
+        className="font-league_spartan text-4xl uppercase font-bold text-ljblue"
+      >
+        heading 2
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
