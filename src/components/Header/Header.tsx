8fce3840517686c5d4b514978dfcb072664ce71e
Mike <mike@mike-francis.org>
2023-05-05 16:31:38 +0100

    checking netlify upload

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
         role="heading"
         className="uppercase font-bold text-3xl"
       >
-        Lemon + Jinja
+        Lemon + Jinja v2
       </div>
       <img role='menu' src='https://via.placeholder.com/36' />
     </div>
