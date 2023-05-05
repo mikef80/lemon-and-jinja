d4e0aee2ec2c6d988c1965c200b8c608ce424f54
Mike <mike@mike-francis.org>
2023-05-05 22:14:22 +0100

    hamburger installed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
       >
         Lemon + Jinja v1
       </div>
-      <img role='menu' src="../images/icons/apple-icon-180.png" className="h-[36px]" />
+      {/* <img role='menu' src="../images/icons/apple-icon-180.png" className="h-[36px]" /> */}
       <HamburgerMenu />
     </div>
   );
