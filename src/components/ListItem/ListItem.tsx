75c1810ed69c87fffb14d8aacead4aea04c3025a
Mike <mike@mike-francis.org>
2023-05-07 20:17:50 +0100

    listitem created. need to work on defining the type of fa icon

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -3,20 +3,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ListItem = () => {
   return (
-    <div role="list-item" className="flex px-4 py-2">
-      <div className="flex-grow">
-        <div className="p-1">Washing Up Liquid</div>
+    <div role="list-item" className="flex px-4 pb-4 pt-3 border-b-[1px]">
+      <div className="pr-2 flex-grow">
+        <div className="">Washing Up Liquid</div>
         <div className="flex">
-          <div className="p-1">
+          <div className="pr-2">
             <FontAwesomeIcon icon="weight-scale" size="xl" />
           </div>
-          <div className="p-1">
-            <input className="border-2 w-[300px]" />
+          <div className="flex-grow">
+            <input type="number" className="border-2 w-full" />
           </div>
         </div>
       </div>
-      <div className="flex justify-center items-center p-1">
-        <FontAwesomeIcon icon="heart" />
+      <div className="flex justify-center items-center ">
+        <FontAwesomeIcon icon="heart" size='2xl' />
       </div>
     </div>
   );
