f592e3cb9bb82101fd7420eae0da6c4d30c9268c
Mike <mike@mike-francis.org>
2023-05-09 21:12:56 +0100

    updated icons

--- vite.config.ts
+++ vite.config.ts
@@ -17,23 +17,23 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
     "start_url": "/",
   "icons": [
       {
-        src: '/src/images/icons/manifest-icon-192.maskable.png',
+        src: 'src/images/icons/manifest-icon-192.maskable.png',
         sizes: '192x192',
         type: 'image/png'
       },
       {
-        src: '/src/images/icons/manifest-icon-512.maskable.png',
+        src: 'src/images/icons/manifest-icon-512.maskable.png',
         sizes: '512x512',
         type: 'image/png'
       },
       {
-        src: '/src/images/icons/apple-icon-180.png',
+        src: 'src/images/icons/apple-icon-180.png',
         sizes: '180x180',
         type: 'image/png',
         purpose: 'apple touch icon'
       },
       {
-        src: '/src/images/icons/manifest-icon-192.maskable.png',
+        src: 'src/images/icons/manifest-icon-192.maskable.png',
         sizes: '225x225',
         type: 'image/png',
         purpose: 'any maskable'
@@ -82,8 +82,8 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
     orientation: 'portrait',
     prefer_related_applications: false
   },
-} */
-
+}
+ */
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
