d8316f27133c770e5d7c1422941f4f7d272c0132
Mike <mike@mike-francis.org>
2023-05-09 21:08:41 +0100

    updated icons

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,31 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
   "display": "standalone",
   "orientation": "portrait",
   "scope": "/",
-  "start_url": "/"
+    "start_url": "/",
+  "icons": [
+      {
+        src: '/src/images/icons/manifest-icon-192.maskable.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/src/images/icons/manifest-icon-512.maskable.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: '/src/images/icons/apple-icon-180.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon'
+      },
+      {
+        src: '/src/images/icons/manifest-icon-192.maskable.png',
+        sizes: '225x225',
+        type: 'image/png',
+        purpose: 'any maskable'
+      },
+    ],
 },
 }
 
