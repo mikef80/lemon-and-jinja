6d759129ea9f2b8ec89f79f5de24cf67f261bfc9
Mike <mike@mike-francis.org>
2023-05-09 17:44:43 +0100

    updated icons

--- vite.config.ts
+++ vite.config.ts
@@ -5,40 +5,40 @@ import react from '@vitejs/plugin-react'
 // PWA STUFF
 const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: 'prompt',
-  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  
   manifest: {
     name: 'Lemon + Jinja',
     short_name: 'Lemon + Jinja',
     description: 'A shopping companion app for Lemon + Jinja',
     icons: [
       {
-        src: '\/src\/images\/icons2\/manifest-icon-192.maskable.png',
+        src: '/src/images/icons2/manifest-icon-192.maskable.png',
         sizes: '192x192',
-        type: 'image\/png'
+        type: 'image/png'
       },
       {
-        src: '\/src\/images\/icons2\/manifest-icon-512.maskable.png',
+        src: '/src/images/icons2/manifest-icon-512.maskable.png',
         sizes: '512x512',
-        type: 'image\/png'
+        type: 'image/png'
       },
       {
-        src: '\/src\/images\/icons2\/apple-icon-180.png',
+        src: '/src/images/icons2/apple-icon-180.png',
         sizes: '180x180',
-        type: 'image\/png',
+        type: 'image/png',
         purpose: 'apple touch icon'
       },
       {
-        src: '\/src\/images\/icons2\/manifest-icon-192.maskable.png',
+        src: '/src/images/icons2/manifest-icon-192.maskable.png',
         sizes: '225x225',
-        type: 'image\/png',
+        type: 'image/png',
         purpose: 'any maskable'
       },
     ],
     theme_color: '#ffffff',
     background_color: '#e8ebf2',
     display: 'standalone',
-    scope: '\/',
-    start_url: '\/',
+    scope: '/',
+    start_url: '/',
     orientation: 'portrait',
     prefer_related_applications: false
   },
