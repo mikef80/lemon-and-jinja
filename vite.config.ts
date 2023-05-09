e11c89ca437ea5adf799b30e9327a5fad45d7ccc
Mike <mike@mike-francis.org>
2023-05-09 21:15:30 +0100

    updated icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,15 @@ import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 
-// PWA STUFF v2
+// PWA STUFF
 const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: 'prompt',
   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
   manifest: {
-  "name": "Lemon + Jinja",
-  "short_name": "Lemon + Jinja",
-  "theme_color": "#ffffff",
-  "background_color": "#ffffff",
-  "display": "standalone",
-  "orientation": "portrait",
-  "scope": "/",
-    "start_url": "/",
-  "icons": [
+    name: 'Lemon + Jinja',
+    short_name: 'Lemon + Jinja',
+    description: 'A shopping companion app for Lemon + Jinja',
+    icons: [
       {
         src: 'src/images/icons/manifest-icon-192.maskable.png',
         sizes: '192x192',
@@ -39,41 +34,6 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
         purpose: 'any maskable'
       },
     ],
-},
-}
-
-// PWA STUFF
-/* const manifestForPlugin: Partial<VitePWAOptions> = {
-  registerType: 'prompt',
-  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-  manifest: {
-    name: 'Lemon + Jinja',
-    short_name: 'Lemon + Jinja',
-    description: 'A shopping companion app for Lemon + Jinja',
-    icons: [
-      {
-        src: '/src/images/icons/manifest-icon-192.maskable.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: '/src/images/icons/manifest-icon-512.maskable.png',
-        sizes: '512x512',
-        type: 'image/png'
-      },
-      {
-        src: '/src/images/icons/apple-icon-180.png',
-        sizes: '180x180',
-        type: 'image/png',
-        purpose: 'apple touch icon'
-      },
-      {
-        src: '/src/images/icons/manifest-icon-192.maskable.png',
-        sizes: '225x225',
-        type: 'image/png',
-        purpose: 'any maskable'
-      },
-    ],
     theme_color: '#ffffff',
     background_color: '#e8ebf2',
     display: 'standalone',
@@ -83,7 +43,7 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
     prefer_related_applications: false
   },
 }
- */
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
