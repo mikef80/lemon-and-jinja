684648add12ffa2e2e8dfefecf903991559379b4
Mike <mike@mike-francis.org>
2023-05-09 22:31:15 +0100

    cleaning up role definitions

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div role="introduction" aria-label="main" className="px-4 py-2">
+    <div role="main" aria-label="main" className="px-4 py-2">
       <h2 className="text-2xl pb-2">Welcome to the unofficial Lemon and Jinja Companion app.</h2>
       <p>Select 'List' to get started adding items to you shopping list or select 'Favourites' to pick items from your favourites list.</p>
 
