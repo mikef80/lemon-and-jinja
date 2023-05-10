a7009ffd9aaee68b930996680269eabe7e47e33d
Mike <mike@mike-francis.org>
2023-05-10 21:37:03 +0100

    favourites working now and adding item numbers

--- src/components/List/listSlice.tsx
+++ src/components/List/listSlice.tsx
@@ -1,39 +1,74 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export interface CounterState {
-    itemCount: number
-    items: {
-        id: number,
-        itemName: string,
-        itemWeight: number,
-        itemFavourite: boolean;
-    }[];
+  itemCount: number;
+  items: {
+    id: number;
+    itemName: string;
+    itemWeight: number;
+    itemFavourite: boolean;
+  }[];
 }
 
 const initialState: CounterState = {
-    itemCount: 0,
-    items: [],
+  itemCount: 0,
+  items: [],
 };
 
 export const listStateSlice = createSlice({
-    name: 'list',
-    initialState,
-    reducers: {
-        addToList: (state, action: PayloadAction<{
-            id: number,
-            itemName: string,
-            itemWeight: number,
-            itemFavourite: boolean;
-        }>) => {
-            state.items.push(action.payload);
-        },
-        incrementCount: (state) => {
-            state.itemCount++;
-        }
+  name: "list",
+  initialState,
+  reducers: {
+    addToList: (
+      state,
+      action: PayloadAction<{
+        id: number;
+        itemName: string;
+        itemWeight: number;
+        itemFavourite: boolean;
+      }>
+    ) => {
+      state.items.push(action.payload);
     },
+    incrementCount: (state) => {
+      state.itemCount++;
+    },
+    updateItem: (
+      state,
+      action: PayloadAction<{ id: number; value: number }>
+    ) => {
+      const { id, value } = action.payload;
+      console.log(typeof id);
+      
+      const index = state.items.findIndex((item) => Number(id) === item.id);
+      console.log(index);
+
+      state.items[index] = { ...state.items[index], itemWeight: value };
+    },
+    updateItemFavourite: (
+      state,
+      action: PayloadAction<{
+        id: number;
+        itemName: string;
+        itemWeight: number;
+        itemFavourite: boolean;
+      }>
+    ) => {
+      const { id } = action.payload;
+
+      const index = state.items.findIndex((item) => item.id === id);
+
+      const item = state.items[index];
+
+      const favourite = item.itemFavourite;
+
+      state.items[index] = { ...item, itemFavourite: !favourite };
+    },
+  },
 });
 
 // Action creators are generated for each case reducer function
-export const { addToList, incrementCount } = listStateSlice.actions;
+export const { addToList, incrementCount, updateItemFavourite, updateItem } =
+  listStateSlice.actions;
 
 export default listStateSlice.reducer;
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,35 +1,68 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { updateItemFavourite, updateItem } from "../List/listSlice";
 
-const ListItem = (props: { key: number, id: number, itemName: string, itemWeight: number, itemFavourite: boolean; }) => {
-    const { id, itemName, itemWeight, itemFavourite } = props;
-    const favourite = itemFavourite ? "fas" : "far";
+const ListItem = (props: {
+  key: number;
+  id: number;
+  itemName: string;
+  itemWeight: number;
+  itemFavourite: boolean;
+}) => {
+  const { id, itemName, itemWeight, itemFavourite } = props;
+  const favourite = itemFavourite ? "fas" : "far";
 
-    const updateFavourite = (e) => {
-        console.log(e);
-        
-    }
+  const dispatch = useAppDispatch();
+  const selected = useAppSelector((state) => state.listState.items[id]);
 
-    return (
-        <li id={id.toString()} role="list-item" className="flex px-4 pb-4 pt-3 border-b-[1px]">
-            <div className="pr-2 flex-grow">
-                <div className="">{itemName}</div>
-                <div className="flex">
-                    <div className="pr-2">
-                        <FontAwesomeIcon icon="weight-scale" size="xl" />
-                    </div>
-                    <div className="flex-grow">
-                        <form action="">
-                            <input name='numberOfItem' type="number" className="border-2 w-full" />
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div className="flex justify-center items-center ">
-                <FontAwesomeIcon icon={[favourite, "heart"]} size='2xl' onClick={updateFavourite} />
-            </div>
-        </li>
-    );
+  const updateFavourite = (e: any) => {
+    dispatch(updateItemFavourite(selected));
+  };
+
+  const updateItemWeight = (e: any) => {
+    e.preventDefault();
+    const id =
+      e.target[0].parentNode.parentNode.parentNode.parentNode.parentNode.id;
+    const payloadObj = {
+      id: id,
+      value: e.target[0].value,
+    };
+    dispatch(updateItem(payloadObj));
+  };
+
+  return (
+    <li
+      id={id.toString()}
+      role="list-item"
+      className="flex px-4 pb-4 pt-3 border-b-[1px]"
+    >
+      <div className="pr-2 flex-grow">
+        <div>{itemName}</div>
+        <div className="flex">
+          <div className="pr-2">
+            <FontAwesomeIcon icon="weight-scale" size="xl" />
+          </div>
+          <div className="flex-grow">
+            <form onSubmit={updateItemWeight}>
+              <input
+                name="numberOfItem"
+                type="number"
+                className="border-2 w-full"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <FontAwesomeIcon
+          onClick={updateFavourite}
+          icon={[favourite, "heart"]}
+          size="2xl"
+        />
+      </div>
+    </li>
+  );
 };
 
 export default ListItem;
