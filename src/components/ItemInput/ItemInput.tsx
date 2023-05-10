9cffe08f8fc7bd6a1a55bb028d834a2a22b010ae
Mike <mike@mike-francis.org>
2023-05-10 13:59:00 +0100

    form submission handler created. Need to created related value in state and update functions

--- src/components/ItemInput/ItemInput.tsx
+++ src/components/ItemInput/ItemInput.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 
 const ItemInput = () => {
-  return (
-    <div>ItemInput</div>
-  )
-}
+  const [input, setInput] = useState('');
 
-export default ItemInput
+  const updateInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const submitInput = (e: FormEvent) => {
+    e.preventDefault();
+    
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitInput} className='w-full h-10'>
+        <input type="text" placeholder='Enter new item...' className='w-full border-x-2 border-b-2 h-full px-4' onChange={updateInput} />
+      </form>
+    </div>
+  );
+};
+
+export default ItemInput;
