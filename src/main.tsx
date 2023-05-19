795d04c6da53cdc2cb35a7a84098af221049e565
Mike <mike@mike-francis.org>
2023-05-19 14:18:01 +0100

    just played with implementing react lazy loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { store } from './app/store'
-import { Provider } from 'react-redux'
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 import Home from "./components/Home/Home";
 import Settings from "./components/Settings/Settings";
-import List from "./components/List/List";
+// import List from "./components/List/List";
+const List = lazy(() => import("./components/List/List"));
 import Favourites from "./components/Favourites/Favourites";
+import Loading from "./components/Loading/Loading";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {index: true, element: <Home />},
-      {path: '/list', element: <List />},
-      {path: '/favourites', element: <Favourites />}
+      { index: true, element: <Home /> },
+      {
+        path: '/list', element: (
+          <Suspense fallback={<Loading />}>
+            <List />
+          </Suspense>)
+      },
+      { path: '/favourites', element: <Favourites /> }
     ]
   },
 ]);
